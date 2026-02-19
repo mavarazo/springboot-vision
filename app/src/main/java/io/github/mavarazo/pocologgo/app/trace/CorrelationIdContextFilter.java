4bd3db38bfd76be59f6233db5a4ee17ca767bf2e
Marco Niederberger <marco.niederberger@gmail.com>
2026-02-19 17:42:31 +0100

    fix: missing correlation id on responses

---
+++ app/src/main/java/io/github/mavarazo/pocologgo/app/trace/CorrelationIdContextFilter.java
@@ -0,0 +1,31 @@
+package io.github.mavarazo.pocologgo.app.trace;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorrelationIdContextFilter extends AbstractContextFilter {
+
+    @Override
+    protected Map<String, String> getContext(final HttpServletRequest request, final HttpServletResponse response) {
+        final Map<String, String> result = new HashMap<>();
+
+        final String correlationId = Objects.requireNonNullElse(request.getHeader("X-Correlation-ID"), UUID.randomUUID().toString());
+        if (StringUtils.hasText(correlationId)) {
+            result.put("correlation.id", correlationId);
+            response.setHeader("X-Correlation-ID", correlationId);
+        }
+
+        return result;
+    }
+}
